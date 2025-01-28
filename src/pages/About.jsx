1effec4310b53e9ee92772eef4e9970674a11294
espadv69 <aitorjob69@gmail.com>
2025-01-28 19:23:07 +0100

    Refactor: Restructure HTML for improved organization and clarity

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,18 +1,24 @@
+import '../css/About.css'
+
 const About = () => {
   return (
-    <div>
-      <h1>About This App</h1>
-      <p>
-        Welcome to the Favorite Quotes Explorer! This app allows you to add,
-        view, and explore your favorite quotes. It demonstrates the power of
-        React and React Router for building dynamic, navigable applications.
-      </p>
-      <h2>Technologies Used:</h2>
-      <ul>
-        <li>React</li>
-        <li>React Router DOM</li>
-        <li>Vite (for fast development and building)</li>
-      </ul>
+    <div className="about">
+      <div>
+        <h1>About This App</h1>
+        <p>
+          Welcome to the Favorite Quotes Explorer! This app allows you to add,
+          view, and explore your favorite quotes. It demonstrates the power of
+          React and React Router for building dynamic, navigable applications.
+        </p>
+      </div>
+      <div>
+        <h2>Technologies Used:</h2>
+        <ul>
+          <li>React</li>
+          <li>React Router DOM</li>
+          <li>Vite (for fast development and building)</li>
+        </ul>
+      </div>
     </div>
   )
 }
