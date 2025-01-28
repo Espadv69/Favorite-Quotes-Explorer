d681114cf89ee2be002245885ad078fac62f24e7
espadv69 <aitorjob69@gmail.com>
2025-01-28 15:44:55 +0100

    Add imports: App component and global css filee

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import App from './App'
+import './css/global.css'
+
 const root = createRoot(document.getElementById('root'))
 
 root.render(
