c0c5b47b41ff6a72ed24fabfd2f67813c231fdfc
espadv69 <aitorjob69@gmail.com>
2025-01-28 18:33:06 +0100

    Feat: Add welcome message for Favorite Quotes Explorer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,13 @@
-const Home = () => {}
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Favorite Quotes Explorer!</h1>
+      <p>
+        Discover, add, and view your favorite quotes. Navigate to the "Quotes"
+        page to start exploring!
+      </p>
+    </div>
+  )
+}
 
 export default Home
