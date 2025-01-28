cae4899f05bb24e0c36a5d7ab92e6033edf65cb5
espadv69 <aitorjob69@gmail.com>
2025-01-28 18:50:40 +0100

    Feat: Implement static quotes array and extract quote ID from URL params

--- src/pages/QuoteDetails.jsx
+++ src/pages/QuoteDetails.jsx
@@ -1,5 +1,19 @@
 import { useParams } from 'react-router-dom'
 
-const QuoteDetails = () => {}
+const QuoteDetails = () => {
+  const { id } = useParams()
+
+  // todo: replace with dynamic data
+  const quotes = [
+    {
+      id: 1,
+      text: 'The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela',
+    },
+    {
+      id: 2,
+      text: 'The way to get started is to quit talking and begin doing. - Walt Disney',
+    },
+  ]
+}
 
 export default QuoteDetails
