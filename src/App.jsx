41cb1c381aaa01d4f3cc9414479b3068d8ce4c98
espadv69 <aitorjob69@gmail.com>
2025-01-28 19:03:56 +0100

    Add classname to div that envolves every components

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import About from './pages/About'
 
 const App = () => {
   return (
-    <div>
+    <div className="components">
       <nav>
         <Link to="/">Home</Link>
         <Link to="/quotes">Quotes</Link>
