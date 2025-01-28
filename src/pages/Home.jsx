71392c457d50481facb5966e7116a85c0b27a992
espadv69 <aitorjob69@gmail.com>
2025-01-28 19:18:32 +0100

    Add import Home css and add classname for home container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
+import '../css/Home.css'
+
 const Home = () => {
   return (
-    <div>
+    <div className="home">
       <h1>Welcome to Favorite Quotes Explorer!</h1>
       <p>
         Discover, add, and view your favorite quotes. Navigate to the "Quotes"
